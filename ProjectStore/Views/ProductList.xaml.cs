6caf7ad8af5c854233d850cb1d12562c0d79dcbd
ammarmangala <ammar.mangala@gmail.com>
2023-02-16 00:57:53 +0100

    Initial commit

---
+++ ProjectStore/Views/ProductList.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectStore.Views;
+
+public partial class ProductList : ContentView
+{
+    public ProductList()
+    {
+        InitializeComponent();
+    }
+}
